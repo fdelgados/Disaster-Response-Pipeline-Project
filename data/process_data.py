2507cdac63ffeba4a1452fc11bb5130fb5ee2f0e
Cisco Delgado <fdelgados@gmail.com>
2019-10-14 20:21:30 +0200

    Drop table if exists before create

--- data/process_data.py
+++ data/process_data.py
@@ -44,7 +44,7 @@ def clean_data(df):
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///{}'.format(database_filename))
 
-    df.to_sql('messages', con=engine, index=False)
+    df.to_sql('messages', con=engine, index=False, if_exists='replace')
 
 
 def main():
