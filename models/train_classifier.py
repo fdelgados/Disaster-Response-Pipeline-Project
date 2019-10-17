c1d3b69881fad05824b377c3cbd7e9945bd1544f
Cisco Delgado <fdelgados@gmail.com>
2019-10-17 14:09:11 +0200

    Add random to train_test_split

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -86,7 +86,7 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
         
         print('Building model...')
         model = build_model()
