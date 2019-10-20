f29a67969f23475ef4b25a71d4ba09ee7fea0705
Cisco Delgado <fdelgados@gmail.com>
2019-10-20 07:19:29 +0200

    Write docstrings

--- data/process_data.py
+++ data/process_data.py
@@ -6,21 +6,53 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """Loads messages and categories data and creates a merged dataframe
+
+    Args:
+        messages_filepath (str): Path to the messages file
+        categories_filepath (str): Path to the categories file
+
+    Returns:
+        (pd.DataFrame): A messages and categories dataframe
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
 
     return messages.merge(categories, on='id')
 
-def remove_url(text, replace=''):
-    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+def replace_url(text, replace=''):
+    """Replaces all urls with the replacement string
+
+    Args:
+        text (str): The string being searched and replaced on
+        replace (str): The replacement value that replaces found urls
+
+    Returns:
+        str: text with the replaced urls
+    """
+    url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
     detected_urls = re.findall(url_regex, text)
     for url in detected_urls:
         text = text.replace(url, replace)
-    
+
     return text
 
+
 def clean_data(df):
+    """
+    Performs the data clean process
+        * Creates a column by category
+        * Drops duplicated rows
+        * Searches and replaces the url from the messages
+        * Drops columns with one unique value
+    Args:
+        df (pd.DataFrame): Dataframe to be cleaned
+
+    Returns:
+        pd.DataFrame: clean dataframe
+    """
     categories = df['categories'].str.split(';', expand=True)
 
     row = categories.iloc[0]
@@ -33,7 +65,7 @@ def clean_data(df):
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].astype(str).str[-1]
-    
+
         # convert column from string to numeric
         numeric_value = pd.to_numeric(categories[column].apply(lambda x: '1' if x == '1' else '0'))
         categories[column] = numeric_value
@@ -47,7 +79,8 @@ def clean_data(df):
     if duplicates > 0:
         df = df.drop_duplicates()
 
-    df['message'] = df['message'].apply(lambda text: remove_url(text))
+    # remove url from messages
+    df['message'] = df['message'].apply(lambda text: replace_url(text))
 
     for column in categories:
         # drop columns with one unique value
@@ -58,6 +91,13 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """
+    Save clean data to a database
+
+    Args:
+        df (pd.DataFrame): clean dataframe
+        database_filename (str): Path to the database file
+    """
     engine = create_engine('sqlite:///{}'.format(database_filename))
 
     df.to_sql('messages', con=engine, index=False, if_exists='replace')
@@ -74,12 +114,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,9 +1,9 @@
 import sys
 import numpy as np
 import pandas as pd
-import pickle
 from sqlalchemy import create_engine
 import nltk
+
 nltk.download('wordnet')
 nltk.download('stopwords')
 
@@ -23,33 +23,49 @@ from sklearn.externals import joblib
 
 stop_words = set(stopwords.words('english'))
 
-def load_data(database_filepath):
-    engine = create_engine('sqlite:///{}'.format(database_filepath))
-    
-    df = pd.read_sql_table('messages', con=engine)
-    
-    X = df['message']
-    Y = df.iloc[:, 4:]
 
-    return X, Y, Y.columns
+def load_data(database_filepath):
+    """Loads data from the database.
+
+    Args:
+        database_filepath (str): Path to the database file
+
+    Returns:
+        tuple: A tuple containing messages as a first element, categories as a second element
+            and a list of category names as the third element
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+
+    df = pd.read_sql_table('messages', con=engine)
+
+    messages = df['message']
+    categories = df.iloc[:, 4:]
+
+    return messages, categories, categories.columns
 
 
 def tokenize(text):
+    """Convert a text to a token list in lowercase and lemmatized. It also eliminates stop words.
+
+    Args:
+        text (str): Text to be converted
+
+    Returns:
+        list: A list of clean tokens
+    """
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
-    clean_tokens = []
-    for token in tokens:
-        if token.lower() in stop_words:
-            continue
-
-        clean_token = lemmatizer.lemmatize(token.strip()).lower()
-        clean_tokens.append(clean_token)
-
-    return clean_tokens
+    return [lemmatizer.lemmatize(token.strip()).lower() for token in tokens
+            if token.lower() not in stop_words]
 
 
 def build_model():
+    """Creates a model with optimized hyperparameters using MultinomialNB estimator
+
+    Returns:
+        A train ready model
+    """
     # ignore all future warnings
     simplefilter(action='ignore', category=FutureWarning)
 
@@ -64,13 +80,19 @@ def build_model():
         'tfidf__use_idf': (True, False),
         'clf__estimator__alpha': (1e-2, 1e-3)
     }
-    
-    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1, verbose=2)
 
-    return cv
+    return GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1, verbose=2)
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """Evaluates the model and displays the accuracy, precision and the F1-score for each category
+
+    Args:
+        model: Model to be evaluated
+        X_test (array): Messages from the test set
+        Y_test (array): Labels from the test set
+        category_names (list|pd.Series): List of category names
+    """
     y_pred = model.predict(X_test)
 
     df_pred = pd.DataFrame(y_pred, columns=category_names)
@@ -82,6 +104,12 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    """Save the trained model to a file
+
+    Args:
+        model: Trained model
+        model_filepath (str): Path to the model file
+    """
     joblib.dump(model, model_filepath, compress=3)
 
 
@@ -91,13 +119,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -107,9 +135,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
